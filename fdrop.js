4366225615373af37f767ae8c9e29a78d819925a
Joe Hitchens <joe@sleepless.com>
2017-06-07 17:19:13 -0700

    -

---
+++ fdrop.js
@@ -0,0 +1,34 @@
+
+(function() {
+
+	var attach = function(element, cb) {
+
+		var style = element.style
+		var old_opacity = style.opacity
+
+		element.ondragenter = function(evt) {
+			style.opacity = "0.5";
+		}
+		element.ondragleave = function(evt) {
+			style.opacity = old_opacity;
+		}
+
+		// for drag/drop to work, element MUST have ondragover and ondrop defined 
+		element.ondragover = function(evt) {
+			evt.preventDefault();			// required: ondragover MUST call this.
+		}
+		element.ondrop = function(evt) {
+			evt.preventDefault();			// required
+			style.opacity = old_opacity;	// because ondragleave not called on drop (chrome at least)
+			var files = evt.dataTransfer.files
+			cb(files, evt);
+		}
+
+	}
+
+	FDrop = {
+		attach: attach,
+	}
+	
+})();
+
